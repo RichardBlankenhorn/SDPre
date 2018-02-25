6ae6686c8ce6d7c067226818d6a7ab048bd8c25c
Richard Blankenhorn <rsbnq6@yahoo.com>
2018-02-24 18:20:39 -0700

    JTHW Exercise 1 (version 3)

--- JavaTheHardWay/FirstProg.java
+++ JavaTheHardWay/FirstProg.java
@@ -1,6 +1,6 @@
 public class FirstProg {
   public static void main( String[] args ) {
-    System.out.println("I am determined to learn how to code.");
+    System.out.println("I , Richard Blankenhorn, am determined to learn how to code.");
     System.out.println("Today's date is 2/24/2018");
   }
 }
