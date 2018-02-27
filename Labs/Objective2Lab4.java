43efb4507a9279ff22f129763ebd4e843278cf21
Richard Blankenhorn <rsbnq6@yahoo.com>
2018-02-26 20:08:49 -0700

    Lab 4 - Variables

---
+++ Labs/Objective2Lab4.java
@@ -0,0 +1,10 @@
+public class Objective2Lab4 {
+  public static void main(String[] args) {
+    double num1 = 2, num2 = 5, num3 = 12, average;
+
+    average = (num1 + num2 + num3) / 3;
+
+    System.out.println("This output would result from numbers " + num1 + ", " +
+    num2 + ", " + num3 + "\n\n\t" + average);
+  }
+}
