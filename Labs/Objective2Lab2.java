593a47906e59738b7f265821cf3114fcda03d08b
Richard Blankenhorn <rsbnq6@yahoo.com>
2018-02-26 20:00:18 -0700

    Lab 2 - Variables

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Richard Blankenhorn";
+    int age = 35;
+    String state = "Missouri";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
