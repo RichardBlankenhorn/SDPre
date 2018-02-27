d3f978d03a3eba2e582c2d6bbf4133d7842bfd36
Richard Blankenhorn <rsbnq6@yahoo.com>
2018-02-26 20:13:14 -0700

    Lab 5 - Variables

---
+++ Labs/Objective2Lab5.java
@@ -0,0 +1,10 @@
+public class Objective2Lab5 {
+  public static void main(String[] args) {
+    double side1 = 4, side2 = 5, hypotenuse;
+
+    hypotenuse = Math.sqrt(side1) + Math.sqrt(side2);
+
+    System.out.println("The hypotenuse of a triangle with sides " + side1 +
+    " and " + side2 + " is " + hypotenuse);
+  }
+}
