c1acb14a49b46fa6ae7c6851aceb44ac429505b0
Richard Blankenhorn <rsbnq6@yahoo.com>
2018-02-26 20:04:08 -0700

    Lab 3 - Variables

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,8 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    String food1 = "Chipotle", food2 = "Chinese", food3 = "Steak";
+
+    System.out.println("My Favorite Foods Are: \n1. " + food1 + "\n2. " + food2 +
+    "\n3. " + food3);
+  }
+}
